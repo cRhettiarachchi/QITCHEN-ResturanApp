b9c22a9095503e0048201f4838bb386ef918b21c
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-22 15:24:32 +0530

    preview is done

--- src/app/dashboard/add-content/add-content.component.ts
+++ src/app/dashboard/add-content/add-content.component.ts
@@ -9,22 +9,39 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class AddContentComponent implements OnInit {
 
   formValue: FormGroup;
+  // imagePreview: string | ArrayBuffer = '';
+  imagePreview: Blob;
 
   constructor() { }
 
   ngOnInit() {
     this.formValue = new FormGroup({
       heading: new FormControl(null,
-        {validators: [Validators.required, Validators.minLength(3)]
+        {validators: [Validators.required, Validators.minLength(3)],
+          updateOn: 'blur'
         }),
       description: new FormControl(null,
-        {validators: [Validators.required, Validators.minLength(10)]
-        })
+        {validators: [Validators.required, Validators.minLength(10)],
+          updateOn: 'blur'
+        }),
+      category: new FormControl('Breakfast'),
+      image: new FormControl(null, {validators: [Validators.required]})
     });
   }
-  FormSubmit(){
-    console.log(this.formValue.value.heading + ' ' + this.formValue.value.description);
-    this.formValue.reset();
+  FormSubmit() {
+    console.log(this.formValue.value.heading + ' ' + this.formValue.value.description + ' ' + this.formValue.value.category);
+    this.formValue.reset({category: 'Breakfast'});
   }
 
+  onImagePick(event: Event) {
+    console.log('this is executed');
+    const file = (event.target as HTMLInputElement).files[0];
+    this.formValue.patchValue({image: file});
+    this.formValue.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
