14aadac9e599b1c56ce7dfa3dba151aea1d36544
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-26 16:13:07 +0530

    creating the registration form

--- src/app/sign-in/register/register.component.ts
+++ src/app/sign-in/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   hide = true;
+  cHide = true;
+  pass: string;
+  confirmPass: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
+    console.log(form);
+  }
+
+  onChange(pass: string, cPass: string) {
+    this.pass = pass;
+    this.confirmPass = cPass;
+    console.log(pass);
   }
 
 }
