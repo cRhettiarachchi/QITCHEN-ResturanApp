52d651fa8529bcec0c44e535d56cebf53c9f2c24
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-28 15:44:50 +0530

    front end access control is done

--- src/app/sign-in/auth.guard.ts
+++ src/app/sign-in/auth.guard.ts
@@ -9,7 +9,7 @@ export class AuthGuard implements CanActivate {
   // tslint:disable-next-line:max-line-length
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const isAuth = this.authService.getAuthStatus();
-    if (!isAuth) {
+    if (!isAuth || localStorage.getItem('type') !== 'admin') {
       this.router.navigate(['/user', 'sign-in']).then(r =>{});
     }
     return isAuth;
