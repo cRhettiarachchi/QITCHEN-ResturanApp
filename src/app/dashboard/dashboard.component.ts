7d3e53d5775dc1e68f728731092bb4d632d62608
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-30 16:19:08 +0530

    added the snack bar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../sign-in/auth.service';
 import {UserModel} from '../models/user.model';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,15 +11,30 @@ import {UserModel} from '../models/user.model';
 export class DashboardComponent implements OnInit {
 
   user: UserModel;
+  isFirstVisit: boolean;
   date = new Date();
   latestDate;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.isFirstVisit = true;
     this.user = this.authService.getUser();
     this.authService.getUserSubject().subscribe(value => {
       this.user = value;
     });
+    if (this.isFirstVisit) {
+      this.openSnackBar('Welcome ' + this.user.name + '!');
+    }
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, ' ', {
+      duration: 5000,
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      panelClass: ['done']
+    });
+    this.isFirstVisit = false;
   }
 
 }
